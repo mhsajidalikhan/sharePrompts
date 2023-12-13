69a0f8d76bf3c23442bc100b092486d5715636bf
Sajid Ali <mhsajidalikhan@gmail.com>
2023-12-13 23:04:32 +0500

    Navigation and layout page settings.

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -1,13 +1,34 @@
+"use client"
 import Link from "next/link";
 import Image from "next/image";
 import { useState, useEffect } from "react";
-import {SignIn, SignOut, userSession, getProviders} from 'next-auth/react';
+import {SignIn, SignOut, userSession, getProviders, signIn, signOut} from 'next-auth/react';
 
 const Nav = () => {
+  const isUserLoggedIn =true;
   return (
-    <div>
-      Nav
-    </div>
+    <nav className='flex-between w-full mb-16 pt-3'>
+      <Link href="/" className="flex gap-2 flex-center">
+        <Image src="/assets/images/logo.svg" 
+        alt = "Logo"
+        width={30}
+        height={30}
+        className="object-contain"
+        />
+         <p className='logo_text'>Promptopia</p>
+      </Link>
+     <div className="sm:flex hidden">
+      {isUserLoggedIn ? (
+        <div className="flex gap-3 md:gap5">
+          <Link href="/create-prompt" className="black_btn">Create Post</Link>
+          <button type="button" onClick={signOut} className="outline_button"> Sign out</button>
+        </div>
+      ):(
+        <>
+        </>
+      )}
+     </div>
+    </nav>
   )
 }
 
